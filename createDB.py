b50b34fae739ec1bf97b379fcd45bbfb8b6b6d40
conzty01 <conzty01@luther.edu>
2017-04-21 21:50:34 -0500

    Updated database creation and populated databases

--- createDB.py
+++ createDB.py
@@ -1,42 +1,60 @@
 import psycopg2
 
-conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
+def createCourse(cursor):
+    cursor.execute("DROP TABLE IF EXISTS lccourse CASCADE;")
+    cursor.execute("""
 
-cur = conn.cursor()
+    CREATE TABLE lccourse (
+        id              serial,
+        num             varchar(50),
+        description     text,
+        title           varchar(100),
+        department      varchar(100),
+        PRIMARY KEY (id)
+    );
 
-cur.execute("""
+    """)
+def createRequirement(cursor):
+    cursor.execute("DROP TABLE IF EXISTS lcrequirement CASCADE;")
+    cursor.execute("""
 
-CREATE TABLE lcCourse (
-    id              serial,
-    num             varchar(50),
-    description     text,
-    title           varchar(100),
-    PRIMARY KEY (id)
-);
+    CREATE TABLE lcrequirement (
+        id              serial,
+        name            varchar(100),
+        description     text,
+        PRIMARY KEY (id)
+    );
 
-""")
+    """)
+    genEds = ["Quantitative","Natural World—Lab","Wellness","Religion","Human Behavior","Human Behavior—Social Science Methods",
+                "Intercultural","Human Expression","Historical","Natural World—Nonlab","Biblical Studies",
+                "Human Expression—Primary Texts","Skills","Paideia111/112","Language","Wellness","Paideia 450","Senior Project",
+                "Ethical"]
 
-cur.execute("""
+    for i in genEds:
+        cursor.execute("INSERT INTO lcrequirement (name) VALUES ('{}')".format(i))
+def createCourseReq(cursor):
+    cursor.execute("DROP TABLE IF EXISTS lccourse_requirement")
+    cursor.execute("""
 
-CREATE TABLE lcRequirement (
-    id              serial,
-    name            varchar(100),
-    description     text,
-    PRIMARY KEY (id)
-);
+    CREATE TABLE lccourse_requirement (
+        course          int,
+        requirement     int,
+        PRIMARY KEY (course, requirement),
+        FOREIGN KEY (course) REFERENCES lcCourse(id),
+        FOREIGN KEY (requirement) REFERENCES lcRequirement(id)
+    );
 
-""")
+    """)
+def main():
+    conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
+    cur = conn.cursor()
 
-cur.execute("""
+    createCourse(cur)
+    createRequirement(cur)
+    createCourseReq(cur)
 
-CREATE TABLE lcCourse_requirement (
-    course          int,
-    requirement     int,
-    PRIMARY KEY (course, requirement),
-    FOREIGN KEY (course) REFERENCES lcCourse(id),
-    FOREIGN KEY (requirement) REFERENCES lcRequirement(id)
-);
 
-""")
+    conn.commit()
 
-conn.commit()
+main()
--- populateDB.py
+++ populateDB.py
@@ -5,7 +5,54 @@ def importFile(f):
     jsonFile = open(f,"r").read()
     return json.loads(jsonFile)
 
+def formatTables(cur):
+    cur.execute("DELETE FROM lccourse_requirement;")
+    cur.execute("DELETE FROM lccourse;")
+
+def popCourse(cur,courseDict):
+    if courseDict["description"] != None:
+        d = courseDict["description"].replace("'","''")
+        executeStr = "INSERT INTO lccourse (num, title, department, description) VALUES ('{}', '{}', '{}', '{}');"\
+                .format(courseDict["number"], courseDict["title"].replace("'","''"), courseDict["subject"], d)
+
+        # if the description is None, then do not include the description so the value is set to null
+    else:
+        executeStr = "INSERT INTO lccourse (num, title, department) VALUES ('{}', '{}', '{}');"\
+                .format(courseDict["number"], courseDict["title"].replace("'","''"), courseDict["subject"])
+
+    cur.execute(executeStr)
+
+def popCourseReq(cur,courseDict):
+    executeStr = ""
+    if len(courseDict["fulfills"]) > 0:
+        for i in range(len(courseDict["fulfills"])):
+            cur.execute("""
+
+                SELECT course.id, req.id
+                FROM (SELECT id FROM lccourse WHERE num = '{}') AS course,
+                     (SELECT id FROM lcrequirement WHERE name = '{}') AS req
+
+            """.format(courseDict["number"],courseDict["fulfills"][i]))
+
+            temp = cur.fetchall()
+
+            cur.execute("INSERT INTO lccourse_requirement (course, requirement) VALUES ('{}','{}')\n"\
+                            .format(temp[0][0],temp[0][1]))
+
+def popDB(conn,obj):
+    cur = conn.cursor()
+    formatTables(cur)
+
+    for item in obj:
+        popCourse(cur, item)
+        popCourseReq(cur, item)
+
 def main():
+    conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
+
     jsonObj = importFile("gened.json")
-    for item in jsonObj:
-        print(item)
+
+    popDB(conn,jsonObj)
+    conn.commit()
+
+main()
