8048c87ed37064963a97c64e7986f8bf548a7341
conzty01 <conzty01@luther.edu>
2017-04-30 22:46:44 -0500

    Bug fixes and added templates

--- genEdServer.py
+++ genEdServer.py
@@ -5,7 +5,7 @@ import json
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-conn = psycopg2.connect(dbname="conzty01",user="conzty01")
+conn = psycopg2.connect(dbname="conzty01",user="conzty01",host="knuth.luther.edu")
 
 @app.route("/")
 def index():
@@ -13,6 +13,45 @@ def index():
 
     cur.execute("SELECT name FROM requirement;")
 
-    return render_template("index.html", requirement=cur.fetchall())
+    return render_template("index.html",requirement=cur.fetchall())
+
+@app.route("/<genEd>")
+def searchGenEd(genEd):
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT num, title, department, name
+        FROM course JOIN course_requirement ON(course.id = course_requirement.course)
+             JOIN requirement ON(course_requirement.requirement = requirement.id)
+        WHERE name LIKE '%%{}%%'
+    """.format(genEd))
+
+    return render_template("result.html",results=cur.fetchall())
+
+@app.route("/searchMult/", methods=["POST"])
+def searchMultiple():
+    cur = conn.cursor()
+    queryStr = ""
+    queryList = []
+
+    for item in request.form:
+        queryList.append(item)
+        queryStr += "'{}' = ANY(req) AND ".format(item)
+
+    queryStr = queryStr[:len(queryStr)-5]
+
+    cur.execute("""
+        SELECT course.num, course.department, c.title, req
+        FROM (SELECT course.title, ARRAY_AGG(requirement.name) AS req
+              FROM course JOIN course_requirement ON(course.id = course_requirement.course)
+			              JOIN requirement ON(course_requirement.requirement = requirement.id)
+              GROUP BY course.title) As c
+              JOIN course ON(c.title = course.title)
+        WHERE {}
+        ORDER BY c.title ASC;
+
+    """.format(queryStr))
+
+    return render_template("result.html",results=cur.fetchall(),ql=queryList)
 
 app.run(debug=True)
